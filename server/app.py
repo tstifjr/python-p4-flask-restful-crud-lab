446fde974dcb827f72384bb08b7a386986698be0
TJ Stifter <tjstifter@gmail.com>
2023-07-26 10:26:22 -0400

    passes tests

--- server/app.py
+++ server/app.py
@@ -47,6 +47,28 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter(Plant.id == id).first()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        r_dict = plant.to_dict()
+        response = make_response(r_dict, 200)
+        return response
+
+    def delete(self,id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        r_dict = {'message': ''}
+        response = make_response(r_dict, 204)
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
